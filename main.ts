cd174524741dac8e9321eef3dc3d48fd2ccfa9e7
GURBEY DEMIRTURK <gurbey61@gmail.com>
2020-11-13 15:36:12 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ let waterQuality = 0
 basic.showString("WQ")
 basic.forever(function () {
     waterQuality = pins.analogReadPin(AnalogPin.P0)
+    // The higher the number, the higher the conductivity, thus more particles present in the water
     if (waterQuality >= 500) {
         basic.showString("TAP")
     } else if (waterQuality < 500 && waterQuality >= 200) {
